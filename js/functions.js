da93a2e074c30d80d42d333f9a5138dd3d81b166
freedfelipe <freedfelipe@gmail.com>
2014-10-02 16:34:12 -0300

    um outro teste

--- js/functions.js
+++ js/functions.js
@@ -1,47 +1,57 @@
+
+function appReady(){  
+    $('#aguarde').attr('style: none;');
+} 
+
 $(document).ready(function() {
-	
-	$.mobile.allowCrossDomainPages = true;
-	
     $.ajax({
         type: 'GET',
 		dataType: 'json',
-        crossdomain: true,
+        crossDomain: true,
         url: 'http://dev.meucarroturbo.com.br/data.php',
         async: false,
 		success: function(response) {
             
-			$('#aguarde').attr('style: none;');
+			var items = [];
 			
-			alert('FOI');
-			
-            //var items = [];
-//            $.each(response, function( key, val) {
-//                
-//                var html = '<li onclick="location.href="single-page.html";">'+
-//							'<img src="'+ val.guid +'" width="200" height="200">'+
-//							'<div class="post-info">'+
-//								'<div class="post-basic-info">'+
-//									'<h3><a href="#">'+ val.post_title +'</a></h3>'+
-//									'<span><a href="#"><label> </label>Carros</a></span>'+
-//									'<p>'+ val.post_content +'</p>'+
-//								'</div>'+
-//								'<div class="post-info-rate-share">'+
-//									'<div class="rateit">'+
-//										'<span> </span>'+
-//									'</div>'+
-//									'<div class="post-share">'+
-//										'<span> </span>'+
-//									'</div>'+
-//									'<div class="clear"> </div>'+
-//								'</div>'+
-//							'</div>'+
-//						'</li>';
-//                
-//                $('#tiles').append(html);
-//            });
+            $.each(response, function( key, val) {
+                
+                var html = '<li onclick="location.href="single-page.html";">'+
+							'<img src="'+ val.guid +'" width="200" height="200">'+
+							'<div class="post-info">'+
+								'<div class="post-basic-info">'+
+									'<h3><a href="#">'+ val.post_title +'</a></h3>'+
+									'<span><a href="#"><label> </label>Carros</a></span>'+
+									'<p>'+ val.post_content +'</p>'+
+								'</div>'+
+								'<div class="post-info-rate-share">'+
+									'<div class="rateit">'+
+										'<span> </span>'+
+									'</div>'+
+									'<div class="post-share">'+
+										'<span> </span>'+
+									'</div>'+
+									'<div class="clear"> </div>'+
+								'</div>'+
+							'</div>'+
+						'</li>';
+                
+                $('#tiles').append(html);
+            });
         },
         error: function(){
             alert("Encontramos algum erro, tente novamente");
         }
     });
 });
+
+document.addEventListener("deviceready", appReady, false);
+
+
+$( document ).bind( "mobileinit", function() {
+	// Make your jQuery Mobile framework configuration changes here!
+	$('#aguarde').attr('style: block;');
+	$.mobile.allowCrossDomainPages = true;
+});
+
+
