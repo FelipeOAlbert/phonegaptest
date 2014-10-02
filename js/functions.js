6be168ef2e5da17f7dccba11154cec02c13fb9e0
freedfelipe <freedfelipe@gmail.com>
2014-10-02 16:21:48 -0300

    Alteracao do js

--- js/functions.js
+++ js/functions.js
@@ -12,31 +12,33 @@ $(document).ready(function() {
             
 			$('#aguarde').attr('style: none;');
 			
-            var items = [];
-            $.each(response, function( key, val) {
-                
-                var html = '<li onclick="location.href="single-page.html";">'+
-							'<img src="'+ val.guid +'" width="200" height="200">'+
-							'<div class="post-info">'+
-								'<div class="post-basic-info">'+
-									'<h3><a href="#">'+ val.post_title +'</a></h3>'+
-									'<span><a href="#"><label> </label>Carros</a></span>'+
-									'<p>'+ val.post_content +'</p>'+
-								'</div>'+
-								'<div class="post-info-rate-share">'+
-									'<div class="rateit">'+
-										'<span> </span>'+
-									'</div>'+
-									'<div class="post-share">'+
-										'<span> </span>'+
-									'</div>'+
-									'<div class="clear"> </div>'+
-								'</div>'+
-							'</div>'+
-						'</li>';
-                
-                $('#tiles').append(html);
-            });
+			alert('FOI');
+			
+            //var items = [];
+//            $.each(response, function( key, val) {
+//                
+//                var html = '<li onclick="location.href="single-page.html";">'+
+//							'<img src="'+ val.guid +'" width="200" height="200">'+
+//							'<div class="post-info">'+
+//								'<div class="post-basic-info">'+
+//									'<h3><a href="#">'+ val.post_title +'</a></h3>'+
+//									'<span><a href="#"><label> </label>Carros</a></span>'+
+//									'<p>'+ val.post_content +'</p>'+
+//								'</div>'+
+//								'<div class="post-info-rate-share">'+
+//									'<div class="rateit">'+
+//										'<span> </span>'+
+//									'</div>'+
+//									'<div class="post-share">'+
+//										'<span> </span>'+
+//									'</div>'+
+//									'<div class="clear"> </div>'+
+//								'</div>'+
+//							'</div>'+
+//						'</li>';
+//                
+//                $('#tiles').append(html);
+//            });
         },
         error: function(){
             alert("Encontramos algum erro, tente novamente");
